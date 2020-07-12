457b9a1cbc1ab4f228b08704fc612586a218bdef
LIshy2 <ishaykhiev2017@litsey2.ru>
2020-07-12 23:19:21 +0300

    Really working wood cutter

--- src/main/java/lishy2/treefarm/entities/WoodCutterBlockEntity.java
+++ src/main/java/lishy2/treefarm/entities/WoodCutterBlockEntity.java
@@ -38,13 +38,12 @@ public class WoodCutterBlockEntity extends TileEntity implements ITickableTileEn
                     cuttingTreeNow = new Tree(this.getPos().up(), world);
                 }
                 Treefarm.LOGGER.info("WORLD STATE " + world);
-                world.destroyBlock(cuttingTreeNow.pop(), true);
+                if (!cuttingTreeNow.isEmpty())
+                    world.destroyBlock(cuttingTreeNow.pop(), true);
                 cooldown = 0;
             }
         }
     }
-
-
 }
 
 class Tree {
@@ -56,6 +55,7 @@ class Tree {
         leaves = new ArrayList<>();
         ArrayDeque<BlockPos> q = new ArrayDeque<>();
         q.add(one);
+        boolean[][][] was = new boolean[400][400][400];
         while (!q.isEmpty()) {
             BlockPos v = q.pollFirst();
             Block vBlock = w.getBlockState(v).getBlock();
@@ -64,28 +64,33 @@ class Tree {
             } else if (vBlock instanceof LeavesBlock) {
                 leaves.add(v);
             } else {
-                break;
+                continue;
             }
+            was[v.getX() - one.getX() + 200][v.getY() - one.getY() + 200][v.getZ() - one.getZ() + 200] = true;
             for (int dx = -1; dx <= 1; ++dx) {
                 for (int dy = -1; dy <= 1; ++dy) {
                     for (int dz = -1; dz <= 1; ++dz) {
                         BlockPos newBlock = v.add(dx, dy, dz);
-                        q.add(newBlock);
+                        if (!was[newBlock.getX() - one.getX() + 200][newBlock.getY() - one.getY() + 200][newBlock.getZ() - one.getZ() + 200]) {
+                            was[newBlock.getX() - one.getX() + 200][newBlock.getY() - one.getY() + 200][newBlock.getZ() - one.getZ() + 200] = true;
+                            q.add(newBlock);
+                        }
                     }
                 }
             }
         }
     }
 
+
     BlockPos peek() {
-        if (!logs.isEmpty()) return logs.get(logs.size() - 1);
         if (!leaves.isEmpty()) return leaves.get(leaves.size() - 1);
+        if (!logs.isEmpty()) return logs.get(logs.size() - 1);
         return null;
     }
 
     BlockPos pop() {
-        if (!logs.isEmpty()) return logs.remove(logs.size() - 1);
         if (!leaves.isEmpty()) return leaves.remove(leaves.size() - 1);
+        if (!logs.isEmpty()) return logs.remove(logs.size() - 1);
         return null;
     }
 
